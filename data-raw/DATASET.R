7fee2ab3f002e95066c423acca90f8c9f7d75714
Kim Dill-McFarland <kdillmcfarland@gmail.com>
2022-03-30 16:24:06 -0700

    Addtl changes for kimma 1.2.0

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,7 +2,7 @@ library(kimma)
 
 #Linear models
 model_result <- kmFit(dat = example.voom, kin = example.kin, patientID = "donorID",
-                      run.lmekin = TRUE, run.lme = TRUE,
+                      run.lmekin = TRUE, run.lme = TRUE, metrics=TRUE,
                       model = "~ virus*asthma + (1|donorID)", processors = 6)
 
 usethis::use_data(model_result, overwrite = TRUE)
