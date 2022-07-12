786532c247aa6e3077b07fc36ee9257b0532cc45
Kimberly Dill-McFarland <kdillmcfarland@gmail.com>
2022-07-11 17:48:54 -0700

    Convert venn plots to ggvenn

--- R/plot_venn_genes.R
+++ R/plot_venn_genes.R
@@ -3,8 +3,9 @@
 #' @param model_result List of data frames output by kimma::kmFit()
 #' @param model Character string of model to plot. Must match object names in model_result. For example, "lm", "lme", "lmekin"
 #' @param variables Character vector of variables in model_result to include in plots
-#' @param contrasts Character vector of contrasts in model_result to include in plots. Only applicable if model name includes 'contrast'
-#' @param intercept Logical if should include the intercept variable
+#' @param contrasts Character vector of contrasts in model_result to include in plots. Format is c("contrast_lvl - contrast_ref", "..."). Only applicable if model name includes 'contrast'
+#' @param intercept Logical if should include the intercept variable. Default is FALSE
+#' @param random Logical if should include random effect variable(s). Default is FALSE
 #' @param fdr.cutoff Numeric vector of FDR cutoffs to assess. One venn per FDR value
 #'
 #' @return List of ggplot objects
@@ -13,37 +14,58 @@
 #' @examples
 #' plot_venn_genes(example_model, model = "lme", fdr.cutoff = c(0.05,0.5))
 
-plot_venn_genes <- function(model_result, model, variables=NULL, intercept=FALSE,
+plot_venn_genes <- function(model_result, model, variables=NULL,
+                            intercept=FALSE, random=FALSE,
                             contrasts=NULL,
                             fdr.cutoff = c(0.05,0.1,0.2,0.3,0.4,0.5)){
 
-  FDR <- variable <- gene <- contrast <- NULL
+  FDR <- variable <- gene <- contrast_ref <- contrast_lvl <- NULL
+
+  #common errors
+  if(!is.null(contrasts) & grepl("contrast", model)){
+    stop("Must provide contrasts model when specifying contrasts.")
+  }
 
   #Extract results
   dat <- model_result[[model]]
 
   #List variables of interest
-  var_all <- unique(dat$variable)
+  if(!is.null(variables)){
+    var_all <- variables
+  } else {
+    var_all <- unique(dat$variable)
+  }
 
-  if(is.null(variables) & intercept){
-    var_filter <- var_all
-  } else if (is.null(variables)){
-    var_filter <- var_all[var_all != "(Intercept)"]
-  } else if (!is.null(variables) & intercept) {
-    var_filter <- unique(c("(Intercept)", variables))
-  } else{
-    var_filter <- variables
+  #remove intercept if specified
+  if(!intercept){
+    var_all <- var_all[var_all != "(Intercept)"]
+  }
+
+  #remove random effects if specified
+  if(!random){
+    var_all <- var_all[!grepl("\\|", var_all)]
   }
 
   #List contrasts of interest
   if(grepl("contrast", model)){
     if(is.null(contrasts)){
-      con_filter <- unique(dat$contrast)
+      con_filter <- dplyr::distinct(dat, contrast_ref, contrast_lvl)
       } else {
-      con_filter <- contrasts
+      con_filter <- strsplit(contrasts, split=" - ") %>%
+        as.data.frame() %>% t() %>%  as.data.frame()
+      colnames(con_filter) <- c("contrast_lvl", "contrast_ref")
+      rownames(con_filter) <- NULL
       }
   }
 
+  #filter data to variables/contrasts of interest
+  dat_filter <- dat %>%
+    dplyr::filter(variable %in% var_all)
+
+  if(grepl("contrast", model)){
+    dat_filter <- dat_filter %>%
+      dplyr::inner_join(con_filter, by = c("contrast_ref", "contrast_lvl"))
+  }
   #List to hold plots
   venn.ls <- list()
 
@@ -51,18 +73,21 @@ plot_venn_genes <- function(model_result, model, variables=NULL, intercept=FALSE
     #list to hold gene vectors
     venn_dat <- list()
     #Significant at chosen FDR
-    dat_filter <- dat %>% dplyr::filter(FDR < fdr)
+    dat_filter_signif <- dat_filter %>% dplyr::filter(FDR < fdr)
 
     #Each variable of interest
     if(!grepl("contrast", model)){
-      for (var in var_filter){
-        venn_dat[[var]] <- dat_filter %>%
+      for (var in var_all){
+        venn_dat[[var]] <- dat_filter_signif %>%
           dplyr::filter(variable == var) %>%
           dplyr::distinct(gene) %>% unlist(use.names = FALSE) }
     } else{
-      for (con in con_filter){
-        venn_dat[[gsub(" ","\n",con)]] <- dat_filter %>%
-          dplyr::filter(contrast == con) %>%
+      for (i in 1:nrow(con_filter)){
+        con.OI <- con_filter[i,]
+
+        venn_dat[[paste(con.OI$contrast_lvl, "-", con.OI$contrast_ref,
+                        sep="\n")]] <- dat_filter_signif %>%
+          dplyr::inner_join(con.OI, by = c("contrast_ref", "contrast_lvl")) %>%
           dplyr::distinct(gene) %>% unlist(use.names = FALSE) }
     }
 
@@ -74,10 +99,10 @@ plot_venn_genes <- function(model_result, model, variables=NULL, intercept=FALSE
 
     #Plot all venns
     if(gene_tot > 0){
-    venn.ls[[as.character(fdr)]] <- venn::venn(ilab=FALSE, zcolor = "style",
-               x=venn_dat, box=FALSE, ggplot = TRUE) +
-      ggplot2::theme(axis.title.x = ggplot2::element_text()) +
-      ggplot2::labs(x=paste("FDR <", fdr))
+    venn.ls[[as.character(fdr)]] <- ggvenn::ggvenn(venn_dat, show_percentage = FALSE,
+           text_size = 4, set_name_size = 4, stroke_size = 0.5) +
+      ggplot2::labs(x = paste("FDR <", fdr)) +
+      ggplot2::theme(axis.title.x = ggplot2::element_text())
     } else {
       print(paste("Zero genes significant at FDR <", fdr))
     }
