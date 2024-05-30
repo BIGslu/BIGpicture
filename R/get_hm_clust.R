64f7fbf74c38b12fb93d5b94530b4ffc5d4aad9b
Kimberly Dill-McFarland <kdillmcfarland@gmail.com>
2024-05-29 17:13:35 -0700

    Update get_hm_clust.R
    Add package leads as needed

--- R/get_hm_clust.R
+++ R/get_hm_clust.R
@@ -16,21 +16,22 @@ get_hm_clust <- function(dat, hm, dimension){
   cluster.result <- data.frame()
 
   # Rows of heatmap
+  hm_rows <- ComplexHeatmap::row_order(hm)
   if(dimension == "row"){
     #Deal with single cluster results
-    if(is.list(row_order(hm))){
-      clust.tot <- length(row_order(hm))
+    if(is.list(hm_rows)){
+      clust.tot <- length(hm_rows)
     } else {
       clust.tot <- 1
     }
 
     for (i in 1:clust.tot){
       #Get row indices
-      if(is.list(row_order(hm))){
+      if(is.list(hm_rows)){
         # version of hm row order needs to be numeric (CHM v2.15.1)
-        cluster.index <- row_order(hm)[[(i)]]
+        cluster.index <- hm_rows[[(i)]]
       } else {
-        cluster.index <- row_order(hm)
+        cluster.index <- hm_rows
       }
 
       #Clusters with >1 element
@@ -46,47 +47,48 @@ get_hm_clust <- function(dat, hm, dimension){
           #Rename default column
           dplyr::rename(row=V1) %>%
           #concatenate results
-          bind_rows(cluster.result)
+          dplyr::bind_rows(cluster.result)
       } else {
         #Clusters with only 1 element
         cluster.result <- data.frame(row = rownames(dat)[cluster.index],
                                      row_within_cluster = 1,
                                      cluster = paste0("cluster", i)) %>%
-          bind_rows(cluster.result)
+          dplyr::bind_rows(cluster.result)
       }
     }
   } else if(dimension == "col"){
     # Columns of heatmap
 
     #Deal with single cluster results
-    if(is.list(column_order(hm))){
-      clust.tot <- length(column_order(hm))
+    hm_cols <- ComplexHeatmap::column_order(hm)
+    if(is.list(hm_cols)){
+      clust.tot <- length(hm_cols)
     } else {
       clust.tot <- 1
     }
 
     for (i in 1:clust.tot){
       #Get column indices
-      if(is.list(column_order(hm))){
-        cluster.index <- column_order(hm)[[(i)]]
+      if(is.list(hm_cols)){
+        cluster.index <- hm_cols[[(i)]]
       } else {
-        cluster.index <- column_order(hm)
+        cluster.index <- hm_cols
       }
 
       #Clusters with >1 element
       if(length(cluster.index) > 1){
         cluster.result <- t(t(colnames(dat[,cluster.index]))) %>%
           as.data.frame() %>%
-          mutate(col_within_cluster = 1:length(cluster.index)) %>%
-          mutate(cluster = paste0("cluster", i)) %>%
+          dplyr::mutate(col_within_cluster = 1:length(cluster.index)) %>%
+          dplyr::mutate(cluster = paste0("cluster", i)) %>%
           dplyr::rename(col=V1) %>%
-          bind_rows(cluster.result)
+          dplyr::bind_rows(cluster.result)
       } else {
         #Clusters with only 1 element
         cluster.result <- data.frame(col = colnames(dat)[cluster.index],
                                      col_within_cluster = 1,
                                      cluster = paste0("cluster", i)) %>%
-          bind_rows(cluster.result)
+          dplyr::bind_rows(cluster.result)
       }
     }
   } else{ stop("dimension must be one of row or col.") }
